a3441d7f89ae62fe5c31f330ba4927c0e774b803
hongzhonglu <luho@chalmers.se>
2019-11-05 15:43:42 +0100

    fix:update file name

---
+++ preprocess_pdb_file.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# -*- python 3 -*-
+# -*- hongzhong Lu -*-
+
+import os
+import pandas as pd
+os.getcwd()
+protein_list = pd.read_excel('/Users/luho/Documents/GitHub/De-nevo-protein-3D-structure-yeast/protein_list.xlsx')
