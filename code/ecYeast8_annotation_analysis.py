a213f2749697ee627676d9242036b5dbed283e34
hongzhonglu <31989005+hongzhonglu@users.noreply.github.com>
2021-12-06 13:34:32 +0800

    feat: add script to do chemostat simulation

---
+++ code/ecYeast8_annotation_analysis.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+'''this code is to carry out the model validation analysis based on yeastGEM with different sources data;
+new gene and new reaction analysis for Yeast8 compared with Yeast7
+5th, Nov, 2018'''
+
+# Import packages
+import pandas as pd
+import numpy as np
+import os    ##for directory
+import sys
+import pprint
+from cobra.io import read_sbml_model, load_matlab_model
+from collections import Counter
+import matplotlib.pyplot as plt
+os.chdir('/Users/xluhon/Documents/GitHub/De-nevo-protein-3D-structure-yeast/code')
+sys.path.append(r"/Users/xluhon/Documents/GitHub/De-nevo-protein-3D-structure-yeast/code")
+pprint.pprint(sys.path)
+
+# import self function
+from mainFunction import *
+
+# input the subsystem information
+gem_dataframe = pd.read_excel('/Users/xluhon/Documents/GitHub/model_correction/result/yeastGEM_with subsystem.xlsx')
+
+# input ecGEM
+ecYeast = load_matlab_model('/Users/xluhon/Documents/GitHub/GECKO2_simulations/ecModels/ecYeastGEM/ecYeastGEM_batch.mat')
+ecYeast = correctSomeWrongFormat(ecYeast)
+#produce the dataframe for the metabolites and the rxn
+gem_met_nov = produceMetaboliteList(ecYeast)
+gem_rxn_nov = produceRxnList(ecYeast)
+gem_gene_nov = produceGeneList(ecYeast)
+
+
+
+
+
+
+# add subsytem
+gem_rxn_nov['subsystem'] = [None]*len(gem_rxn_nov['equation'])
+#obtain the transport reaction
+gem_rxn_nov['subsystem'] = transport(gem_rxn_nov['formula'].tolist(), gem_rxn_nov['subsystem'].tolist())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#the following is not used!
+#remove the transport reaction
+gem_rxn_transport = gem_rxn_nov[gem_rxn_nov['subsystem'].str.contains('Transport', na=False)]
+gem_rxn_metabolic = gem_rxn_nov[gem_rxn_nov['subsystem'].str.contains('Transport', na=False)==False]
+#remove the exchange reaction
+gem_rxn_metabolic1 = gem_rxn_metabolic[gem_rxn_metabolic['name'].str.contains('exchange', na=False)==False]
+#remove the arm reaction
+gem_rxn_metabolic2 = gem_rxn_metabolic1[gem_rxn_metabolic1['rxnID'].str.contains('arm', na=False)==False]
+#all metabolic reaction
+gpr = gem_rxn_metabolic2['GPR'].tolist()
+gpr[1]
+#obtain number of metabolic reaction with gpr
+rxn_gpr = [x for x in gpr if x !='']
+#obtain the number of metabolic reaction without gpr
+len(gpr)-len(rxn_gpr)
+
+#calculate the number of Promiscuous enzymes
+#remove the reaction with isoenyzme
+gem_rxn_nov1 = gem_rxn_nov[gem_rxn_nov['GPR'].str.contains('or', na=False)==False]
+gem_rxn_nov2 = gem_rxn_nov1[gem_rxn_nov1['GPR'].str.contains('and', na=False)==False]
+#remove the prot_ reaction
+gem_rxn_nov3 = gem_rxn_nov2[gem_rxn_nov2['rxnID'].str.contains('prot_', na=False)==False]
+promoiscuous = pd.Series(gem_rxn_nov3['GPR']).value_counts()
+promoiscuous0 = promoiscuous[promoiscuous >=2]
+len(promoiscuous0)-1
--- code/ecYeast_simulation.py
+++ code/ecYeast_simulation.py
@@ -1,2 +1,212 @@
 # this module is mainly for ecModel simulation
 # which model should be used?
+# first compare the model difference
+# version 1
+from cobra.io import read_sbml_model, load_matlab_model
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import sys
+import pprint
+os.chdir('/Users/xluhon/Documents/GitHub/De-nevo-protein-3D-structure-yeast/code')
+sys.path.append(r"/Users/xluhon/Documents/GitHub/De-nevo-protein-3D-structure-yeast/code")
+pprint.pprint(sys.path)
+
+# import self function
+from mainFunction import *
+
+# function copy from strain_design repo
+def ecYeastMinimalMedia(model):
+    """
+    This function is used to define a simple media for ecYeast
+    :param model:
+    :return: a model with the defined the minimal media
+    """
+    rxnID = []
+    rxnName = []
+    for i, x in enumerate(model.reactions):
+        rxnID.append(x.id)
+        rxnName.append(x.name)
+
+    exchange_rxn =[x for x, y in zip(rxnID, rxnName) if '_REV' in x and 'exchange' in y]
+    # first block any uptake
+    for i, x in enumerate(exchange_rxn):
+        rxn0 = exchange_rxn[i]
+        #print(rxn0)
+        model.reactions.get_by_id(rxn0).upper_bound = 0
+
+    #Allow uptake of essential components
+    model.reactions.get_by_id("r_1654_REV").upper_bound = 10000 #ammonium exchange (reversible)
+    model.reactions.get_by_id("r_1861_REV").upper_bound = 10000 #iron(2+) exchange (reversible)
+    model.reactions.get_by_id("r_2100_REV").upper_bound = 10000 #water exchange (reversible)
+    model.reactions.get_by_id("r_1992_REV").upper_bound = 10000 #oxygen exchange (reversible)
+    model.reactions.get_by_id("r_2005_REV").upper_bound = 10000 #phosphate exchange (reversible)
+    model.reactions.get_by_id("r_2060_REV").upper_bound = 10000 #sulphate exchange (reversible)
+    model.reactions.get_by_id("r_1832_REV").upper_bound = 10000 #H+ exchange (reversible)
+    return model
+
+def frange(start, stop, step):
+    """
+    This function is like range, step can be float value, like 0.1
+    :param start:
+    :param stop:
+    :param step:
+    :return:
+    """
+    i = start
+    while i < stop:
+        yield i
+        i += step
+
+# chemostat simulations
+def chemostatSimulation(model0, D0):
+    growth = D0
+    with model0:
+        model0 = ecYeastMinimalMedia(model0)
+        # set growth
+        model0.reactions.get_by_id("r_2111").bounds = (growth, growth)
+        # minimization glucose uptake rate
+        model0.reactions.get_by_id("r_1714_REV").bounds = (0, 1000)  # open the glucose
+        model0.objective = {model0.reactions.r_1714_REV: -1}
+        solution2 = model0.optimize()
+        GR = solution2.fluxes["r_1714_REV"]  # get the glucose uptake rate
+        model0.reactions.get_by_id("r_1714_REV").bounds = (GR, GR * 1.001)
+        model0.objective = {model0.reactions.prot_pool_exchange: -1}
+        solution3 = model0.optimize()
+    return solution3
+dir1 = "/Users/xluhon/Documents/GitHub/GECKO2_simulations/ecModels/ecYeastGEM/ecYeastGEM_batch.mat"
+
+ecYeast = load_matlab_model(dir1)
+
+# reaction annotation
+# r_2111 growth
+# r_1714_REV glucose uptake
+# r_1992_REV oxygen uptake
+# r_1672 co2 production
+# r_1761 ethanol production
+# r_1634 acetate secretion
+
+
+# simulation based on the minimization of protein abundances
+model = ecYeast.copy()
+growth = [0.01, 0.02, 0.04, 0.06, 0.08, 0.1, 0.2, 0.3, 0.31, 0.32, 0.33, 0.34, 0.35, 0.36]
+glucose_uptake = []
+oxgen_uptake = []
+co2_production = []
+ethanol_production = []
+acetate_production = []
+protein_pool = []
+model.reactions.get_by_id("r_1634").upper_bound = 0 # assume acetate is not produced!
+model.reactions.get_by_id("r_2033").upper_bound = 0 # assume pyruvate is not produced!
+model.reactions.get_by_id("r_1631").upper_bound = 0 # assume acetaldehyde is not produced!
+model.reactions.get_by_id("r_1549").upper_bound = 0 # assume (R,R)-2,3-butanediol is not produced!
+
+gem_rxn_nov = produceRxnList(model)
+
+for i in growth:
+    print(i)
+    if i < 0.36:
+        string0 = "D=" + str(i)
+        solution = chemostatSimulation(model0=model, D0=i)
+        oxgen_uptake.append(solution.fluxes["r_1992_REV"])
+        co2_production.append(solution.fluxes["r_1672"])
+        ethanol_production.append(solution.fluxes["r_1761"])
+        acetate_production.append(solution.fluxes["r_1634"])
+        growth.append(solution.fluxes["r_2111"])
+        glucose_uptake.append(solution.fluxes["r_1714_REV"])
+        protein_pool.append(solution.fluxes['prot_pool_exchange'])
+        gem_rxn_nov[string0]=list(solution.fluxes)
+    else:
+        break
+
+# plot the relation between the glucose uptake rate and growth rate
+growth = growth[0:13]
+plt.figure()
+plt.plot(growth, glucose_uptake, marker='.', label='Glucose')
+plt.plot(growth, oxgen_uptake, marker='.',label='O2')
+plt.plot(growth, co2_production, marker='.',label='CO2')
+plt.plot(growth, ethanol_production, marker='.',label='ethanol')
+plt.plot(growth, acetate_production, marker='.',label='acetate')
+plt.xlabel('Growth rate (/h)')
+plt.ylabel('rate (mmol/gDW.h)')
+plt.legend(loc='upper left')
+plt.ylim(0, 25)
+plt.show()
+
+# from the above simulation it could find the secretion of acetate
+gem_rxn_nov.to_excel("../result/flux_check.xlsx")
+
+plt.figure()
+plt.plot(growth, protein_pool, marker='.',label='protein_pool')
+plt.xlabel('Growth rate (/h)')
+plt.ylabel('rate (mmol/gDW.h)')
+plt.legend(loc='upper left')
+plt.show()
+# from the above simulation it could find the secretion of acetate
+
+
+
+
+
+
+
+
+
+
+
+# FBA analysis
+model = ecYeast.copy()
+model.objective = 'r_4041' # r_2111
+model.reactions.get_by_id("r_1714_REV").bounds = (0, 5)
+solution1 = model.optimize()
+model.summary()
+
+#combine the fluxes with rxn information
+flux0 = pd.DataFrame(solution1.fluxes)
+flux1 = flux0.rename_axis("rxnID").reset_index()
+gem_rxn_nov = produceRxnList(model)
+gem_rxn_nov["flux"] = flux1["fluxes"]
+gem_rxn_nov["rxnID2"] = flux1["rxnID"]
+gem_rxn_nov.to_excel("../result/flux_manual_check.xlsx")
+
+# simulate the growth
+"""analysis based on general FBA"""
+# Open the glucose uptake
+glucose_uptake = list(frange(0, 15, 0.2))
+growth = []
+oxgen_uptake = []
+co2_production = []
+ethanol_production = []
+acetate_production = []
+protein_pool = []
+model0 = ecYeast.copy()
+model0 = ecYeastMinimalMedia(model0)
+model0.objective = 'r_2111'
+model0.reactions.get_by_id("r_2111").lower_bound = 0
+model0.reactions.get_by_id("r_1634").upper_bound = 0 # assume acetate is not produced!
+
+for i in glucose_uptake:
+    model0.reactions.get_by_id("r_1714_REV").bounds = (0, i) #D-glucose exchange (reversible)
+    solution = model0.optimize()
+    oxgen_uptake.append(solution.fluxes["r_1992_REV"])
+    co2_production.append(solution.fluxes["r_1672"])
+    ethanol_production.append(solution.fluxes["r_1761"])
+    acetate_production.append(solution.fluxes["r_1634"])
+    growth.append(solution.fluxes["r_2111"])
+    protein_pool.append(solution.fluxes['prot_pool_exchange'])
+
+# plot the relation between the glucose uptake rate and growth rate
+plt.figure()
+plt.plot(growth, glucose_uptake, marker='.', label='Glucose')
+plt.plot(growth, oxgen_uptake, marker='.',label='O2')
+plt.plot(growth, co2_production, marker='.',label='CO2')
+plt.plot(growth, ethanol_production, marker='.',label='ethanol')
+plt.plot(growth, acetate_production, marker='.',label='acetate')
+plt.xlabel('Growth rate (/h)')
+plt.ylabel('rate (mmol/gDW.h)')
+plt.legend(loc='upper left')
+plt.show()
+# from the above simulation it could find the secretion of acetate
+
+
+
