7981242d70a256aacc027ffcd677a79c3a30d846
hongzhonglu <31989005+hongzhonglu@users.noreply.github.com>
2022-03-17 11:58:48 +0800

    fix: refine omics analysis pipeline

--- code/omics/3.4 omics_inferred_size_all_datasets.py
+++ code/omics/3.4 omics_inferred_size_all_datasets.py
@@ -10,28 +10,10 @@ from src.model_process import *
 from src.mainFunction import *
 from src.protein_process import *
 
-
-# protein abundance combine and analysis
-protein_abundance = pd.read_excel("data/proteomics/omics_measured_combine.xlsx")
-protein_copy = pd.read_excel("data/proteomics/protein_copy_combine.xlsx")
-# change the mmol/gDW as molecular/cell
-coefficient1 = 7.8298e9
-column0 = protein_abundance.columns
-column1 = column0[1:]
-protein_abundance1 = protein_abundance[column1]
-protein_abundance2 = protein_abundance1.copy()
-for x in column1:
-    print(x)
-    protein_abundance2[x] = protein_abundance1[x]*coefficient1
-
-protein_abundance2['gene'] = protein_abundance['all_gene']
-# combine the two dataframe
-protein_copy_all = pd.merge(left=protein_abundance2, right=protein_copy, left_on=['gene'], right_on=['gene'], how='outer')
-column2 = protein_copy_all.columns
-column20 = [x for x in column2 if x is not 'gene']
-column21 = ['gene'] + column20
-protein_copy_all1 = protein_copy_all[column21]
-protein_copy_all1.to_excel("data/proteomics/all_protein_copy.xlsx")
+# input the protein abundance data
+protein_copy_all1 = pd.read_excel("data/proteomics/all_protein_copy.xlsx")
+column20 = list(protein_copy_all1.columns)
+column20 = [x for x in column20 if x != "gene"]
 
 
 # input the pro structure size data
@@ -66,57 +48,6 @@ for col0 in column20:
     result2[col0] = value2
 result1.to_excel("data/proteomics/volume_size_across_compartment.xlsx")
 result2.to_excel("data/proteomics/membrane_size_across_compartment.xlsx")
-
-
-
-
-
-
-# only analyze the rosemary datasets under NH4 limitation?
-Sample_ID_select = ['prot.1','prot.2', 'prot.3','prot.7','prot.8','prot.9','prot.10','prot.11','prot.12','prot.13','prot.14','prot.15','prot.16','prot.17','prot.18','prot.19','prot.20','prot.21']
-protein_copy_all1 = protein_copy_all1[Sample_ID_select+["gene"]]
-compartment = getCompartmentGeneList(filter="Yes") # based on the automatic way
-all_compartment = list(compartment.keys())
-# compartment curation
-# here for the following two membrane, only the compartment annotation with experimental evidence is used.
-# also for the fungal-type vacuole membrane, some proteins belong to the metabolic enzymes with high abundance were removed.
-# all_compartment = ['plasma membrane']
-gene_plasma_membrane = pd.read_excel("data/gene_belong_plasma_membrane_annotations.xlsx")
-# all_compartment = ['fungal-type vacuole membrane']
-gene_fungal_type_vacuole_membrane = pd.read_excel("data/gene_belong_fungal_type_vacuole_membrane_annotations.xlsx")
-result1 = pd.DataFrame({"compartment": all_compartment})
-result2 = pd.DataFrame({"compartment": all_compartment})
-for col0 in Sample_ID_select:
-    print(col0)
-    value1=[]
-    value2=[]
-    for y in all_compartment:
-        print(y)
-        location0 = y
-        pro_abundance = protein_copy_all1[['gene',col0]]
-        pro_abundance.columns = ['gene','molecular/cell']
-        if y == "plasma membrane":
-            genes_select = gene_plasma_membrane["gene"].tolist()# for the test
-        elif y == "fungal-type vacuole membrane":
-            genes_select = gene_fungal_type_vacuole_membrane["gene"].tolist()  # for the test
-            genes_select = [x for x in genes_select if x not in ["YAL005C","YLL024C"]] # remove two genes for fungal type vacuole membrane
-        else:
-            genes_select = compartment[y]
-        pro_abundance1 = getProAundance(genes_select0=genes_select, pro_abundance0=pro_abundance)
-        if pro_abundance1 is "no_abundance":
-            value1.append(None)
-            value2.append(None)
-        else:
-            x, S = getStructureSize_MeasuredAbundances(pro_size0=pro_size, abundance0=pro_abundance1)
-            value1.append(x)
-            value2.append(S)
-    result1[col0] = value1
-    result2[col0] = value2
-result1.to_excel("data/proteomics/volume_size_across_compartment_Rosemary_NH4_limitation.xlsx")
-result2.to_excel("data/proteomics/membrane_size_across_compartment_Rosemary_NH4_limitation.xlsx")
-
-
-
 
 
 
@@ -296,12 +227,13 @@ result2.to_excel("data/proteomics/membrance_size_across_go_term.xlsx")
 
 
 # other for specific reactions - check the glucose transporter abundance
+protein_abundance = pd.read_excel("data/proteomics/omics_measured_combine.xlsx")
 glucose_transporter = pd.read_excel("data/glucose_transporter_abundance_check.xlsx")
 glucose_transporter = glucose_transporter[glucose_transporter['Note'].isna()]
 omics_glucose_transporter = protein_abundance[protein_abundance['all_gene'].isin(glucose_transporter['gene'])]
 omics_glucose_transporter.to_excel("data/omics_glucose_transporter.xlsx")
 
-
+protein_copy = pd.read_excel("data/proteomics/protein_copy_combine.xlsx")
 glucose_transporter_copy = protein_copy[protein_copy['gene'].isin(glucose_transporter['gene'])]
 glucose_transporter_copy['section_area'] = singleMapping(glucose_transporter['section_area'],glucose_transporter['gene'],glucose_transporter['gene'])
 # get the section area
--- code/omics/3.5 omics_inferred_size_Only_Rosemary.py
+++ code/omics/3.5 omics_inferred_size_Only_Rosemary.py
@@ -11,28 +11,8 @@ from src.mainFunction import *
 from src.protein_process import *
 
 
-# protein abundance combine and analysis
-protein_abundance = pd.read_excel("data/proteomics/omics_measured_combine.xlsx")
-protein_copy = pd.read_excel("data/proteomics/protein_copy_combine.xlsx")
-# change the mmol/gDW as molecular/cell
-coefficient1 = 7.8298e9
-column0 = protein_abundance.columns
-column1 = column0[1:]
-protein_abundance1 = protein_abundance[column1]
-protein_abundance2 = protein_abundance1.copy()
-for x in column1:
-    print(x)
-    protein_abundance2[x] = protein_abundance1[x]*coefficient1
-
-protein_abundance2['gene'] = protein_abundance['all_gene']
-# combine the two dataframe
-protein_copy_all = pd.merge(left=protein_abundance2, right=protein_copy, left_on=['gene'], right_on=['gene'], how='outer')
-column2 = protein_copy_all.columns
-column20 = [x for x in column2 if x is not 'gene']
-column21 = ['gene'] + column20
-protein_copy_all1 = protein_copy_all[column21]
-protein_copy_all1.to_excel("data/proteomics/all_protein_copy.xlsx")
-
+# input the protein abundance data
+protein_copy_all1 = pd.read_excel("data/proteomics/all_protein_copy.xlsx")
 
 # input the pro structure size data
 pro_size = pd.read_excel("result/sce_protein_size_3D_structure.xlsx")
