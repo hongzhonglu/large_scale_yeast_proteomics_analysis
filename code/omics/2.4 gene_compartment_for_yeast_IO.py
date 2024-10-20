df3979437d69e2e7861c774af5290a99f3319e1b
hongzhonglu <31989005+hongzhonglu@users.noreply.github.com>
2024-10-20 10:28:39 +0800

    fix: refine some code name

--- code/omics/2.4 gene_compartment_for_yeast_IO.py
+++ code/omics/2.4 gene_compartment_for_yeast_IO.py
@@ -154,8 +154,9 @@ new_compartment["source"] = "MULocDeep"
 
 
 #get sce gene annotation
-compartment_dict20 = getCompartmentGeneList(type="all") # Remove some compartmental annotation only with computational evidence (keep experimental evidence)
+compartment_dict20 = getCompartmentGeneList(type="all")
 compartment_dict20_update =gene_location_curation_sce(compartment_dict20)
+
 # change it as a list
 sce_gene_list = []
 for x, y in compartment_dict20_update.items():
